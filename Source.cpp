d92732136ec60188ff5853edffd52dc61ebf79af
Joe <bruiserjoe01@gmail.com>
2020-09-25 14:03:02 -0500

    Added music

--- Source.cpp
+++ Source.cpp
@@ -1,44 +1,46 @@
 #define OLC_PGE_APPLICATION
 #include <iostream>
-#include <string.h>
 #include <time.h>
 #include "olcPixelGameEngine.h"
-
-// Credit to Javidx9 for OlcPixelGameEngine
-// Download it here 
-// https://github.com/OneLoneCoder/olcPixelGameEngine
+#include <Windows.h>
+//these two headers are already included in the <Windows.h> header
+#pragma comment(lib, "Winmm.lib")
 
 class Dropping {
 public:
-	int x;
-	float y;
+	int32_t x;
+	int32_t y;
 	int speed;
 	int w;
 	int h;
 };
 
+
 bool paused = false;
-int px = 400;
-int py = 300;
+bool exitf = false;
+float px = 400.0f;
+float py = 300.0f;
 int score = 0;
 int dn = 0;
 int dif = 100;
-Dropping drop[100];
 float fTargetFrameTime = 1.0f / 500.0f; // Virtual FPS of 100fps
 float fAccumulatedTime = 0.0f;
-
-
+Dropping drop[300];
 
 void death() {
+	mciSendString("play mp3 repeat", NULL, 0, NULL);
 	px = 400;
 	py = 300;
 	score = 0;
 	dn = 0;
 	dif = 100;
-	for (int j = 0; j < 100; j++) {
+	for (int j = 0; j < 300; j++) {
 		drop[j].y = 800;
 	}
+
 }
+//
+
 
 
 
@@ -49,9 +51,14 @@ public:
 		sAppName = "Box Dodge";
 	}
 
+private:
+	int maint;
+	int deatht;
 public:
 	bool OnUserCreate() override{
-		
+		//use wasapi
+		mciSendString("open \"boxdodge.mp3\" type mpegvideo alias mp3", NULL, 0, NULL);
+		mciSendString("play mp3 repeat", NULL, 0, NULL);
 		return true;
 	}
 	bool OnUserUpdate(float fElapsedTime) override{
@@ -61,7 +68,7 @@ public:
 			fAccumulatedTime -= fTargetFrameTime;
 			fElapsedTime = fTargetFrameTime;
 
-			if (dn == 99) {
+			if (dn == 299) {
 				dn = 0;
 			}
 			if (score % 1000 == 0) {
@@ -85,6 +92,9 @@ public:
 			if (GetKey(olc::Key::D).bHeld && paused == false) {
 				px+=(500*fElapsedTime);
 			}
+			if (GetKey(olc::Key::ESCAPE).bHeld) {
+				return false;
+			}
 			if (score % dif == 0) {
 				drop[dn].x = rand() % 800;
 				drop[dn].y = 0;
@@ -105,7 +115,7 @@ public:
 			else if (py > 600) {
 				py = 580;
 			}
-			for (int i = 0; i < 99; i++) {
+			for (int i = 0; i < 299; i++) {
 				if (drop[i].x < px + 20 && drop[i].x + drop[i].w > px && drop[i].y < py + 20 && drop[i].y + drop[i].h > py) {
 					paused = true;
 					break;
@@ -122,6 +132,7 @@ public:
 			DrawRect(px, py, 20, 20, olc::BLUE);
 			DrawString(700, 20, "Score: " + s);
 			if (paused) {
+				mciSendString("stop mp3", NULL, 0, NULL);
 				DrawString(400, 200, "You died. Your score is: " + s, olc::RED);
 				DrawString(400, 160, "Press space to start again", olc::RED);
 				if (GetKey(olc::Key::SPACE).bHeld) {
@@ -135,7 +146,6 @@ public:
 			return true;
 		}
 	}
-
 };
 
 
@@ -143,7 +153,7 @@ public:
 int main() {
 	srand(time(NULL));
 	BoxD box;
-	if (box.Construct(800, 600, 1, 1, false, false)) {
+	if (box.Construct(800, 600, 1, 1, true, false)) {
 		box.Start();
 	}
 	return 0;
